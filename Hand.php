f74a7f813c3afced7699f9a9e152eb99b2cbaae5
Brandon Savage <brandon@brandonsavage.net>
2010-04-23 11:44:49 -0400

    Adding additional files. There's not much to this at this point.

---
+++ Hand.php
@@ -0,0 +1,7 @@
+<?php
+
+class Hand
+{
+  protected $visibleCards;
+  protected $round;
+}
--- Table.php
+++ Table.php
@@ -1,7 +1,54 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-?>
+class Table
+{
+  protected $hand;
+  protected $players = array();
+  protected $dealer;
+  protected $littleBlind;
+  protected $bigBlind;
+  protected $pot;
+
+  public function addDealer(Dealer $dealer)
+  {
+    $this->dealer = $dealer;
+  }
+  
+  public function addPlayer(Player $player)
+  {
+    if(count($players) >= 10) {
+      throw new Exception('Sorry, all the seats are taken at this Table');
+    }
+
+    array_push($this->players, $player);
+  }
+
+  public function removePlayer($key)
+  {
+    if(isset($this->players[$key])) {
+      return $this->players[$key];
+      unset($this->players[$key]);
+    }
+  }
+
+  public function beginHand()
+  {
+
+  }
+
+  public function determineWinner()
+  {}
+
+  public function endHand()
+  {
+    $this->littleBlind = false;
+    $this->bigBlind = false;
+    $this->pot = 0;
+    $this->visibleCards = array();
+  }
+
+  // Function that handles betting, asks the dealer to do the dealer's job,
+  // and maintains state.
+  public function processRound()
+  {}
+}
